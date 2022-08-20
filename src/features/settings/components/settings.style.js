3b648378423ced6ff8f96cbbb8dc4fcc5406f033
NeginOlfat <neginolfat@yahoo.com>
2022-08-20 12:49:48 +0430

    update setting screen

--- src/features/settings/components/settings.style.js
+++ src/features/settings/components/settings.style.js
@@ -4,6 +4,7 @@ import { SafeArea } from "../../../components/utility/safe-area.component";
 
 export const SettingsItem = styled(List.Item)`
     padding: ${props => props.theme.space[3]};
+    background-color: rgba(255,255,255,0.4)
 `;
 
 export const AvatarContainer = styled.View`
@@ -13,4 +14,16 @@ export const AvatarContainer = styled.View`
 export const NoFavouritesArea = styled(SafeArea)`
     align-items: center;
     justify-content: center;
+`;
+
+export const SettingsBackground = styled.ImageBackground.attrs({
+    source: require("../../../../assets/home_bg.jpg")
+})`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
+export const TransparentSafeArea = styled(SafeArea)`
+  background-color: transparent;
 `;
--- src/features/settings/screens/setting.screen.js
+++ src/features/settings/screens/setting.screen.js
@@ -4,11 +4,10 @@ import { List, Avatar } from "react-native-paper";
 import { useFocusEffect } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import { SafeArea } from "../../../components/utility/safe-area.component";
 import { Text } from "../../../components/typography/text.component";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
-import { SettingsItem, AvatarContainer } from "../components/settings.style";
+import { SettingsItem, AvatarContainer, SettingsBackground, TransparentSafeArea } from "../components/settings.style";
 import { colors } from "../../../infrastructure/theme/colors";
 
 
@@ -30,37 +29,52 @@ export const SettingsScreen = ({ navigation }) => {
     );
 
     return (
-        <SafeArea>
-            <AvatarContainer>
-                <Spacer position="top" size="large">
-                    <TouchableOpacity
-                        onPress={() => navigation.navigate("Camera")}
-                    >
-                        {!photo && (
-                            < Avatar.Icon size={180} icon="human" backgroundColor={colors.brand.primary} />
-                        )}
-                        {photo && (
-                            < Avatar.Image size={180} source={{ uri: photo }} />
-                        )}
-                    </TouchableOpacity>
-                </Spacer>
-                <Spacer position="top" size="large">
-                    <Text variant="label">{user.email}</Text>
-                </Spacer>
-            </AvatarContainer>
-            <List.Section>
-                <SettingsItem
-                    title="Favourites"
-                    description="View your Favourites"
-                    left={() => <List.Icon color="black" icon="cards-heart" />}
-                    onPress={() => navigation.navigate("Favourites")}
-                />
-                <SettingsItem
-                    title="Logout"
-                    left={() => <List.Icon color="black" icon="logout" />}
-                    onPress={onLogout}
-                />
-            </List.Section>
-        </SafeArea>
+        <SettingsBackground>
+            <TransparentSafeArea>
+                <AvatarContainer>
+                    <Spacer position="top" size="large">
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate("Camera")}
+                        >
+                            {!photo && (
+                                < Avatar.Icon size={180} icon="human" backgroundColor={colors.brand.primary} />
+                            )}
+                            {photo && (
+                                < Avatar.Image size={180} source={{ uri: photo }} />
+                            )}
+                        </TouchableOpacity>
+                    </Spacer>
+                    <Spacer position="top" size="large">
+                        <Text variant="label">{user.email}</Text>
+                    </Spacer>
+                </AvatarContainer>
+                <List.Section>
+                    <SettingsItem
+                        title="Favourites"
+                        description="View your Favourites"
+                        left={() => <List.Icon color={colors.ui.error} icon="cards-heart" />}
+                        onPress={() => navigation.navigate("Favourites")}
+                    />
+                    <Spacer />
+                    <SettingsItem
+                        title="Payment"
+                        left={() => <List.Icon color={colors.ui.secondary} icon="cart" />}
+                        onPress={() => null}
+                    />
+                    <Spacer />
+                    <SettingsItem
+                        title="Past Order"
+                        left={() => <List.Icon color={colors.ui.secondary} icon="history" />}
+                        onPress={() => null}
+                    />
+                    <Spacer />
+                    <SettingsItem
+                        title="Logout"
+                        left={() => <List.Icon color={colors.ui.secondary} icon="logout" />}
+                        onPress={onLogout}
+                    />
+                </List.Section>
+            </TransparentSafeArea>
+        </SettingsBackground>
     );
 };
